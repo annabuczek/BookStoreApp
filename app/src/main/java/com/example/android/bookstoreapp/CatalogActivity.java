01fcd2bb6d8ad19893bd791eb6fdf1f06c57b48a
Anna Buczek <aania.buczek@gmail.com>
2018-07-14 16:48:10 +0200

    Layout for activity_edit

--- app/src/main/java/com/example/android/bookstoreapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/bookstoreapp/CatalogActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.bookstoreapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        FloatingActionButton fabButton = findViewById(R.id.fab_add);
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CatalogActivity.this, EditActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
