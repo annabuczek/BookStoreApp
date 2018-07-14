222e60be84e5cfd63af33a0c0d93b072f273db91
Anna Buczek <aania.buczek@gmail.com>
2018-07-14 23:40:41 +0200

    Create BookContract class

---
+++ app/src/main/java/com/example/android/bookstoreapp/data/BookContract.java
@@ -0,0 +1,59 @@
+package com.example.android.bookstoreapp.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for Book Store App.
+ */
+
+public class BookContract {
+
+    /** Private constructor as this class should never be instantiated */
+    private BookContract(){}
+
+    /**
+     * Inner class that defines constant values for book catalog database.
+     * Each row in the table represent one product.
+     */
+    public class BookCatalogEntry implements BaseColumns {
+
+        /** Name of the database table for book products */
+        public static final String TABLE_NAME = "book_catalog";
+
+        /** Unique ID number for a product
+         *
+         * Type: INTEGER
+         */
+        public static final String _ID = BaseColumns._ID;
+
+        /** Product Name f.e title of the book.
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_PRODUCT_NAME = "product_name";
+
+        /** Price for the product.
+         *
+         * Type: INTEGER
+         */
+        public static final String COLUMN_PRICE = "price";
+
+        /** Amount of the products.
+         *
+         * Type: INTEGER
+         */
+        public static final String COLUMN_QUANTITY = "quantity";
+
+        /** Product supplier name.
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+
+        /** Product supplier phone number.
+         *
+         * Type: TEXT
+         */
+        public static final String COLUMN_SUPPLIER_PHONE = "supplier_phone_number";
+    }
+}
